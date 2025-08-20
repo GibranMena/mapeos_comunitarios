2cb69bca620627a934a60bc38363b0983f288bdf
Gibran Mena <gibran.mena@gmail.com>
2025-08-20 15:06:32 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Mapeos_Comunitarios',
+    version='0.1.0',
+    author='Gibran Mena-Aguilar',
+    description='A project for a full data funneling andd visualization of community harvested maps',
+    python_requires='>=3',
+    license='GNU General Public License v3',
+    packages=find_packages(),
+)
